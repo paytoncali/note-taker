025c97737e3e03f2430d876e910574baf8530cc0
Payton Whinnery <paytoncaliw@gmail.com>
2021-03-19 14:44:18 -0500

    got routes working, starting functionality for writing and posting notes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,20 +1,36 @@
 const notesData = require('../db/db.json');
 const generateUniqueId = require('generate-unique-id');
+const router = require('express').Router();
+const fs = require('fs');
 
-module.exports = (app) => {
-    app.get("/api/notes", (req, res) => res.json(notesData));
-    app.post("/api/notes", (req, res) => {
+
+router.get("/notes", (req, res) => 
+    res.json(notesData));
+
+    fs.readFile()
+
+router.post("/notes", (req, res) => {
         notesData.push(req.body);
         res.json(true);
         
+    fs.writeFile();
+        notesData.push.JSON.stringify(req.body);
+        
+    fs.readFile();
+
         const id = generateUniqueId({
             length: 3,
             useLetters: false,
         })
     });
+
     // app.post("/api/clear", (req, res) => {
     //     tableData.length = 0;
     //     waitlistData.length = 0;
     //     res.json({ok: true});
     // });
-};
+
+
+
+    module.exports = router;
+
---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(__dirname + '/public'))
+
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
+app.use('/', htmlRoutes);
+app.use('/api', apiRoutes);
+
+app.listen(PORT, () => {
+  console.log(`App listening on PORT: ${PORT}`);
+});
